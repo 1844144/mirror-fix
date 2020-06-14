61155e8e0219ece5beadefdebd629aab51bed37a
vl <1844144@gmail.com>
2020-06-14 13:57:18 +0300

    added monaco

--- fix.js
+++ fix.js
@@ -1,11 +1,1 @@
-require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.20.0/min/vs'}});
-require(['vs/editor/editor.main'], function() {
-                var editor = monaco.editor.create(document.querySelector('.code-editor'), {
-                        value: [
-                                'function x() {',
-                                '\tconsole.log("Hello world!");',
-                                '}'
-                        ].join('\n'),
-                        language: 'javascript'
-                });
-});
+console.log(monaco);
